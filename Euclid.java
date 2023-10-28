6d4bbc908f54ebe9e5ef80ded27fa2f4f14174fb
zuhaIrshad123 <148038984+zuhaIrshad123@users.noreply.github.com>
2023-10-28 06:59:07 -0700

    added a Euclid file

---
+++ Euclid.java
@@ -0,0 +1,28 @@
+public class Euclid {
+
+    // recursive implementation
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+    }
+
+    // non-recursive implementation
+    public static int gcd2(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+
+    // main method
+    public static void main(String[] args) {
+        int p = Integer.parseInt(args[0]);
+        int q = Integer.parseInt(args[1]);
+        int d = gcd(p, q);           //recursion
+        int d2 = gcd2(p, q);         // while loop
+        System.out.println("gcd(" + p + " , " + q + " ) = " + d);
+        System.out.println("gcd(" + p + " , " + q + " ) = " + d2);
+    }
+}
